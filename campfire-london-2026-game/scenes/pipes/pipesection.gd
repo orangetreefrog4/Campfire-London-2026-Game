b1113887bcf406e626eb75d104b98692103f0c5d
Liam Owen <123574555+orangetreefrog4@users.noreply.github.com>
2026-02-28 16:27:41 +0000

    Update the update!

--- campfire-london-2026-game/scenes/pipes/pipesection.gd
+++ campfire-london-2026-game/scenes/pipes/pipesection.gd
@@ -8,6 +8,7 @@ func _ready() -> void:
 func _on_next_trigger_body_entered(body: Node2D) -> void:
 	print("NEXT!")
 	g.player_speed += 0.1
-	print("New speed is " + g.player_speed)
+	print("Speed: ")
+	print(g.player_speed)
 	repipe_pipes.emit()
 	get_node("Moving").set("position", Vector2((get_node("Moving").get("position").x + 1088),372))
