21a8ef2d34070c59d32006e0480fa128a41fa4c4
Liam Owen <123574555+orangetreefrog4@users.noreply.github.com>
2026-02-28 16:31:33 +0000

    Reloading

--- campfire-london-2026-game/globals.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var player_position = Vector2(0,0)
-
-# Will increase every time pipe is passed.
-var player_speed = 2.5
--- campfire-london-2026-game/scenes/pipes/player_collider.gd
+++
@@ -1,4 +0,0 @@
-extends StaticBody2D
-
-func _physics_process(delta: float) -> void:
-	global_position = g.player_position
